f47b926eb34713b154138c5168333a431810aee5
jibolacodes <jibzsupreme@gmail.com>
2022-06-17 13:04:50 +0100

    commited

--- src/App.js
+++ src/App.js
@@ -1,43 +1,111 @@
 import React from 'react'
 import './App.css'
 import Dice from './Dice'
+import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function App() {
 
   // Generating an object of 10 random numbers
   const [dice, setDice] = React.useState(diceNumbers())
+  const [click, setClick] = React.useState(0)
+  const [previous, setPrevious] = React.useState(0)
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+    // Checking if all the dice is held . i.e all die.isHold == true
+    const allHeld = dice.every(die => die.isHold)
+
+    // Using the first die value to check all other dice values
+    const firstValue = dice[0].value
+    const equalValue = dice.every(die => firstValue === die.value)
+
+    // checking if all dice are held and have equal values
+    if (allHeld && equalValue) {
+      setTenzies(true)
+      console.log("You WON!")
+    }
+  }, [dice])
+
+  
+  React.useEffect(() => {
+    const btn = document.querySelector(".roll-button")
+    btn.addEventListener("click", function() {
+      setClick(oldClick => oldClick + 1)
+    })
+  }, [])
+  
+  // Generate dice 
+  function generateDie() {
+    return ({
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHold: false
+    })
+  }
   
   function diceNumbers() {
     const numbers = []
     for (let i = 0; i < 10; i++) {
-      numbers.push(
-        {
-          id: `${i}id`,
-          value: Math.ceil(Math.random() * 6),
-          isHold: true
-        }
-      )
+      numbers.push(generateDie())
     }
     return numbers
   }
 
   // Generating new dice
   const die = dice.map(die => {
-    return <Dice key={die.id} value={die.value}/>
+    return <Dice 
+      key={die.id} 
+      value={die.value} 
+      isHold={die.isHold}
+      holdDice = {() => holdDice(die.id)}
+    />
   })
 
-  function rollDice() {
+  // Roll Dice
+  function rollDice(id) {
+    if (tenzies === false){
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHold ? die : generateDie()
+      }))
+      setPrevious(click)
+    } else {
+      setTenzies(false)
       setDice(diceNumbers)
+      setClick(0)
+    }
   }
+
+    // Holding Dice
+    function holdDice(id) {
+      setDice(oldDice => oldDice.map(die => {
+        return die.id === id ?
+        {...die, isHold : !die.isHold} :
+        die
+      }))
+    }
+  
 
   return (
     <main>
-      <div className='board'>
-        <h2 className='title'>Tenzies</h2>
-        <div className='die-container'>
-          {die}
+      <div className='score--board'>
+        <h2 className='score'>{tenzies ? "Your Score" : "Clicks"} : {click}</h2>
+        {
+        click === 0 &&
+        <h2 className='score previous'>Previous Score : {previous}</h2>
+        }
+      </div>
+      <div className='container'>
+        {/* Render Confetti when tenzies game is won */}
+        {tenzies && <Confetti />}
+        
+        <div className='board'>
+          <h2 className='title'>Tenzies</h2>
+          <div className='die-container'>
+            {die}
+          </div>
+          <button onClick={rollDice} className='roll-button'>{tenzies ? "Replay" : "Roll Dice"}</button>
         </div>
-        <button onClick={rollDice} className='roll-button'>Roll Dice</button>
       </div>
     </main>
   )
--- src/Dice.js
+++ src/Dice.js
@@ -1,6 +1,13 @@
 export default function Dice(props) {
+    const styles = {
+        backgroundColor : props.isHold ? "#55AA55" : "white"
+    }
     return (
-        <div className='die die__selected'>
+        <div 
+            className='die' 
+            style={styles}
+            onClick={props.holdDice}
+        >
             <p className='die--number'>{props.value}</p>
         </div>
     )
